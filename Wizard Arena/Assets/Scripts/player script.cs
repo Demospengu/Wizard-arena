cd0568652fdc198ab817597127e3b0d109aae0da
sandersj13 <jaylen02sanders@gmail.com>
2025-04-08 10:30:44 -0500

    movement

--- Wizard Arena/Assets/Scripts/player script.cs
+++ Wizard Arena/Assets/Scripts/player script.cs
@@ -5,20 +5,27 @@ using UnityEngine;
 public class CharacterMovement : MonoBehaviour
 {
 
+    public float moveSpeed = 5f;
+    private Rigidbody2D rb;
+
 // Start is called before the first frame update
     void Start()
     {
+        rb = GetComponent<Rigidbody2D>();
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        float moveX = Input.GetAxis("Horizontal");
-        float moveZ = Input.GetAxis("Vertical");
 
+        float moveInput = Input.GetAxisRaw("Horizontal");
        
-         
+       
+
+
+        rb.velocity = new Vector2(moveInput * moveSpeed, rb.velocity.y);
+      
         
     }
 }
