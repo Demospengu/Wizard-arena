200013551ca82f0a03cd1e7cb061d566be1aec2a
sandersj13 <jaylen02sanders@gmail.com>
2025-04-09 09:47:08 -0500

    jump script

--- Wizard Arena/Assets/Scripts/animController.cs
+++ Wizard Arena/Assets/Scripts/animController.cs
@@ -4,20 +4,64 @@ using UnityEngine;
 
 public class animationController : MonoBehaviour
 {
+
+    public float jumpForce = 1f;
+    public float jumpInterval = 0.5f;
+    private bool isGrounded;
+    private Rigidbody2D rb;
+
+   
+    
+
+    
+
     Animator animController;
     // Start is called before the first frame update
     void Start()
     {
         animController = GetComponent<Animator>();
+
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        
         if (Input.GetKey(KeyCode.RightArrow))
         {
             animController.SetInteger("control", 1);
 
+
+
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+
+                Jump();
+            }
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            JumpContinuously();
         }
     }
-}
+
+    void JumpContinuously()
+    {
+        rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+
+    }
+        void Jump()
+
+        {
+
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        }
+
+    }
+
+    
+    
+
