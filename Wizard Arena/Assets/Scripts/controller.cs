cde792c6b5b8e518136359e156885e6725105576
Demospengu <demospenguin@gmail.com>
2025-04-17 09:48:09 -0500

    manager edit

--- Wizard Arena/Assets/Scripts/controller.cs
+++ Wizard Arena/Assets/Scripts/controller.cs
@@ -6,41 +6,15 @@ using UnityEngine.SceneManagement;
 
 public class controller : MonoBehaviour
 {
-    public GameObject[] popUps;
-    private int popUpIndex;
     // Start is called before the first frame update
     void Start()
     {
-        SceneManager.LoadScene("tutorial", LoadSceneMode.Additive);
+        SceneManager.LoadScene("Starter Level", LoadSceneMode.Additive);
     }
 
     // Update is called once per frame
     void Update()
     {
-        for (int i = 0; i < popUps.Length; i++)
-        {
-            if (i == popUpIndex)
-            {
-                popUps[popUpIndex].SetActive(true);
-            }
-            else
-            {
-                popUps[popUpIndex].SetActive(false);
-            }
-        }
-        if (popUpIndex == 0)
-        {
-            if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.RightArrow))
-            {
-                popUpIndex++;
-            }
-            else if (popUpIndex == 1)
-            {
-                if (Input.GetKeyDown(KeyCode.UpArrow))
-                {
-                    popUpIndex++;
-                }
-            }
-        }
+        
     }
 }
