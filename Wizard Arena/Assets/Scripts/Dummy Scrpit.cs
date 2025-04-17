b767b6c14caa214d532f2bd06f0456291ddf6a3e
sandersj13 <jaylen02sanders@gmail.com>
2025-04-17 09:48:59 -0500

    tutorial

--- Wizard Arena/Assets/Scripts/Dummy Scrpit.cs
+++ Wizard Arena/Assets/Scripts/Dummy Scrpit.cs
@@ -1,20 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
-
-public class controller : MonoBehaviour
+public class DummyScrpit : MonoBehaviour
 {
-   
     // Start is called before the first frame update
     void Start()
     {
-        SceneManager.LoadScene("Starter Level", LoadSceneMode.Additive);
+        
     }
 
     // Update is called once per frame
     void Update()
     {
+        
     }
 }
