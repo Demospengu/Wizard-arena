f1d7a51610ce29698761e2fc1bbdf099912a6823
sj12028802 <Jonathans4523@gmail.com>
2025-04-21 09:52:00 -0500

    wizard

--- Wizard Arena/Assets/Scripts/controller.cs
+++ Wizard Arena/Assets/Scripts/controller.cs
@@ -6,11 +6,7 @@ using UnityEngine.SceneManagement;
 
 public class controller : MonoBehaviour
 {
-<<<<<<< HEAD
-   
-=======
->>>>>>> 1888d6b6558d55db1fa3cfe146ac146be02d3262
-    // Start is called before the first frame update
+
     void Start()
     {
         SceneManager.LoadScene("Starter Level", LoadSceneMode.Additive);
@@ -19,9 +15,6 @@ public class controller : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-<<<<<<< HEAD
-=======
-        
->>>>>>> 1888d6b6558d55db1fa3cfe146ac146be02d3262
+
     }
 }
